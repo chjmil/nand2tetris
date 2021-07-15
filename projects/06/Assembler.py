d98b3e790b3cd07274e660f5f965e8ecb957f6ca
chjmil <christopher.miller0301@gmail.com>
2021-07-15 00:44:19 -0400

    Completed first part of Assembler
    I have completed the first part of the assembler.
    It can currently translate an ASM file with no variables
    into the binary file. However there are still a few items
    that need to be fixed:
    
    * It needs to be able to read variables
    * It needs to take a filepath as input

--- projects/06/Assembler.py
+++ projects/06/Assembler.py
@@ -1,5 +1,6 @@
 import os
 from enum import Enum
+import re
 
 class instruction(Enum):
     A_INSTRUCTION = 0
@@ -11,25 +12,92 @@ class Parser():
     """
 
     def __init__(self, input_file):
-        self.input: str = input_file
-        self.has_more_lines: bool = False
+        self.input: str = ""
+        self.current_line = 0
+        self.has_more_lines: bool = True
         self.instructionType: instruction = instruction.A_INSTRUCTION
         self.symbol: str = ""
         self.dest: str = ""
         self.comp: str = ""
         self.jump: str = ""
+        self.comp_map = {
+            '0': '0101010',
+            '1': '0111111',
+            '-1': '0111010',
+            'D': '0001100',
+            'A': '0110000',
+            'M': '1110000', 
+            '!D': '0001101',
+            '!A': '0110001',
+            '!M': '1110001', 
+            '-D': '0001111',
+            '-A': '0110011',
+            '-M': '1110011', 
+            'D+1': '0011111',
+            'A+1': '0110111',
+            'M+1': '1110111', 
+            'D-1': '0001110',
+            'A-1': '0110010',
+            'M-1': '1110010', 
+            'D+A': '0000010',
+            'D+M': '1000010', 
+            'D-A': '0010011',
+            'D-M': '1010011', 
+            'A-D': '0000111',
+            'M-D': '1000111', 
+            'D&A': '0000000',
+            'D&M': '1000000', 
+            'D|A': '0010101',
+            'D|M': '1010101', 
+        }
 
-    def advance():
-        pass
+        # read input file
+        with open(input_file, 'r') as f:
+            self.input =  f.readlines()
+        
+        self.advance()
 
-    def get_instruction_type(self):
-        return self.instructionType
-    
+    def advance(self):
+        """
+        Loop through the file and translate it
+        """
+        out = []
+        # 111accccccdddjjj
+        # c_instruct_regex = re.compile(r'111(\d)(\d{6})(\d{3})(\d{3})')
+        # dest = comp ; jmp - dest and jmp are optional
+        c_instruct_regex = re.compile(r"((.*?)=)?([^;\n]+)(;(.*))?")
+        for line in self.input:
+            if line[:2] == '//' or line == '\n':
+                # comment - ignore and don't increment
+                continue
+            elif line[0] == '@':
+                # symbol
+                # TODO this doesn't work for variables
+                bin_num = bin(int(line[1:]))
+                out.append(f"{bin_num[2:]:0>16}")
+            else:
+                # Instruction
+                sections = c_instruct_regex.match(line)
+                # 2 = dest, 3 = comp, 5 = JMP
+                dest = self.get_dest(sections[2])
+                comp = self.get_comp(sections[3])
+                jmp = self.get_jump(sections[5])
+                c_instruct = f"111{comp}{dest}{jmp}"
+                out.append(c_instruct)                
+
+            # increment and loop
+            self.current_line += 1
+        
+        for i in out:
+            print(i)
+            
     def get_dest(self, input):
         """
         Returns the symbolic dest part of the current C-instruction
         """
         out = 0
+        if input == None:
+            return '000'
         if 'M' in input:
             out += 1
         if 'D' in input:
@@ -37,19 +105,19 @@ class Parser():
         if 'A' in input:
             out += 4
         # Convert to binary, substring out the '0b', pad to 3 characters
-        return f"{str(bin(out))[2:]}:0>3"
+        return f"{str(bin(out))[2:]:0>3}"
 
     def get_comp(self, input):
         """
         Returns the symbolic comp part of the current C-instruction
         """
-        pass
+        return self.comp_map[input]
 
     def get_jump(self, input):
         """
         Returns the symbolic jump part of the current C-instruction
         """
-        if 'null' in input:
+        if input == None:
             out='000'
         elif 'JGT' in input:
             out = '001'
@@ -67,7 +135,6 @@ class Parser():
             out = '111'
         return out
 
-    
 class SymbolTable():
 
     def __init__(self):
@@ -99,3 +166,6 @@ class SymbolTable():
         @return address associated with symbol
         """
         return self.symbol_table.get(symbol, "")
+
+if __name__ == "__main__":
+    parser = Parser("max/MaxL.asm")
